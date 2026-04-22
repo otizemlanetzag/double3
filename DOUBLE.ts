ae97a9d98e22416cec60f90bbd10fa2749fcf4a3
otizemlanetzag <urielhetzroni@gmail.com>
2026-04-22 11:35:36 +0300

    DOUBLE.ts bijwerken

--- DOUBLE.ts
+++ DOUBLE.ts
@@ -2,9 +2,9 @@ let onSyncAction: (name: string, value: number) => void;
 let remoteStorage: { [key: string]: number } = {};
 
 /**
- * חיבור לרדיו
+ * חיבור לפרויקט בקבוצת רדיו
  */
-//% color="#4b7bec" icon="\uf0c9" block="connect to project on group %id"
+//% color="#4b7bec" icon="\uf0c9" block="connect to group %id"
 function connectToProject(id: number): void {
     radio.setGroup(id);
 }
@@ -18,15 +18,15 @@ function syncVariable(name: string, value: number): void {
 }
 
 /**
- * בלוק אירוע - הפעם בלי Namespace כדי למנוע שגיאות כפילות
+ * בלוק אירוע - ללא שמות המשתנים בכותרת כדי למנוע שגיאות
  */
-//% color="#4b7bec" icon="\uf0c9" block="on variable received"
+//% color="#4b7bec" icon="\uf0c9" block="on received"
 //% draggableParameters="reporter"
 function onVariableReceived(handler: (name: string, value: number) => void) {
     onSyncAction = handler;
 }
 
-// ניהול קבלת הנתונים
+// קבלת הנתונים מהרדיו
 radio.onReceivedValue(function (receivedName, receivedValue) {
     if (receivedName.indexOf("s:") == 0) {
         let cleanName = receivedName.substr(2);
@@ -36,41 +36,3 @@ radio.onReceivedValue(function (receivedName, receivedValue) {
         }
     }
 })
-
-/**
-* Gebruik dit bestand om specifieke functies en blokken te definiëren.
-* Lees meer op https://makecode.microbit.org/blocks/custom
-*/
-
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
-
-/**
- * Custom blocks
- */
-//% weight=100 color=#0fbc11 icon=""
-namespace custom {
-    /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
-    //% block
-    export function foo(n: number, s: string, e: MyEnum): void {
-        // Add code here
-    }
-
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
-    }
-}
